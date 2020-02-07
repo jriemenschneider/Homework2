4a79038232b47ba32c0e9e1855a5207a1be7d42b
jriemenschneider <jriemenschneider@luc.edu>
2020-02-06 19:01:24 -0600

    Drafed-putS-and-main-functions

--- src/term.c
+++ src/term.c
@@ -2,11 +2,14 @@
 
 
 putS(char *s){
-
-
+	volatile char *video = (volatile char*)0xB8000;
+	while( *string != 0){
+		*video++ = *string++;
+	}
 }
 
 main(){
-
+	char str[] = "HELLO";
+	putS(str);
 
 }
