5cd7f12a7570f59a1f3a991e9864c388b3647a3c
jriemenschneider <jriemenschneider@luc.edu>
2020-02-17 00:35:46 -0600

    Got-Next-Line-Working-Correctly

--- src/term.c
+++ src/term.c
@@ -1,24 +1,26 @@
- 
-
 
 unsigned int x_pos = 0, y_pos = 0;
 void puts(char *s){
-	char *terminalBuffer = 0xB8000;
-        int k;
-        for(k=0; k<10; k++){
-                terminalBuffer[y_pos*80+x_pos] = s;//need to increment x and y
+	volatile char *terminalBuffer= 0xB8000;
+        while(*s != 0){
+		if(x_pos>=79 || *s == '\n'){
+                        x_pos = 0;
+                        terminalBuffer += 160;
+                        //y_pos= y_pos+160;//goes down like Y
+			
                 }
+		else{
+		terminalBuffer[y_pos*80+x_pos] = *s;//terminal buffer is na array and the index you right to is the location you want(which is in []) !!! so it's an array of shorts bc those are each two bytes long!)
+		x_pos = x_pos+2;
+		}
+		s++;
+		//need to write to terminalBuffer like its an array and increment x and y 
+                
+		}
 
 } 
 int main() {
-	char str = 'K';
+	char str[] = "Hello\nI'm tired.";
 	puts(str);
-	/*char *terminalBuffer = 0xB8000;
-        int k;
-        for(k=0; k<10; k++){
-                terminalBuffer[y_pos*80+x_pos] = 'k';//need to increment x and y
-                }*/
-
-
 }
 
